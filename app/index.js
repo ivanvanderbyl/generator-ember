9ba14db0f5ab6ab8d85e46b73ba938ac9c2a1e85
Ivan Vanderbyl <ivanvanderbyl@me.com>
2013-02-16 12:09:46 +1000

    Added extra dirs

---
+++ app/index.js
@@ -0,0 +1,55 @@
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+
+
+module.exports = Generator;
+
+function Generator() {
+  yeoman.generators.Base.apply(this, arguments);
+  this.sourceRoot(path.join(path.dirname(__dirname), 'templates'));
+}
+
+util.inherits(Generator, yeoman.generators.Base);
+
+Generator.prototype.createDirLayout = function createDirLayout() {
+  this.mkdir('app');
+  this.mkdir('app/scripts');
+  this.mkdir('app/styles');
+  this.mkdir('app/images');
+
+  this.mkdir('app/scripts/controllers');
+  this.mkdir('app/scripts/models');
+  this.mkdir('app/scripts/views');
+  this.mkdir('app/scripts/routes');
+  this.mkdir('app/scripts/templates');
+};
+
+Generator.prototype.git = function git() {
+  this.copy('gitignore', '.gitignore');
+  this.copy('gitattributes', '.gitattributes');
+};
+
+Generator.prototype.jshint = function jshint() {
+  this.copy('jshintrc', '.jshintrc');
+};
+
+Generator.prototype.editorConfig = function editorConfig() {
+  this.copy('editorconfig', '.editorconfig');
+};
+
+Generator.prototype.packageJSON = function packageJSON() {
+  this.template('package.json');
+};
+
+Generator.prototype.indexFile = function createIndexFile() {
+  this.template('index.html', 'app/index.html');
+};
+
+Generator.prototype.styleFile = function createStyleFile() {
+  this.template('styles/style.css', 'app/styles/style.css');
+};
+
+Generator.prototype.gruntFile = function createGruntFile() {
+  this.template('Gruntfile.js', 'Gruntfile.js');
+};
