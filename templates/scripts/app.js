ffda403574de996de896e8eb9437ea1675650df7
Ivan Vanderbyl <ivanvanderbyl@me.com>
2013-02-16 13:06:11 +1000

    Stubbed out a basic app

--- templates/scripts/app.js
+++ templates/scripts/app.js
@@ -1,1 +1,22 @@
 var App = Em.Application.create();
+
+App.reopen({
+  Store: DS.Store.extend({
+    revision: 11,
+    adapter: DS.FixtureAdapter.create()
+  })
+});
+
+App.Router.map(function() {
+  this.resource('contacts', function() {
+    this.resource('contact', {path: '/:contact_id'} function() {
+      this.route('edit')
+    });
+  });
+});
+
+App.IndexRoute = Ember.Route.extend({
+  setupController: function() {
+    console.log("INDEX")
+  }
+})
